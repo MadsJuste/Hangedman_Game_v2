00476d918e7e66c49935b467875d089315b96b2a
Mads Justesen <madsjuste89@gmail.com>
2016-01-19 16:28:39 +0100

    Added Userlogic

---
+++ app/src/main/java/com/example/juste/hangedman_game_v2/UserLogic.java
@@ -0,0 +1,20 @@
+package com.example.juste.hangedman_game_v2;
+
+/**
+ * Created by Juste on 19-01-2016.
+ */
+public class UserLogic {
+    private String name;
+    private int score;
+
+
+    public UserLogic() {}
+
+    public String getName(){
+        return name;
+    }
+    public int getScore(){return score;}
+    public String toString(){
+        return "name = "+ name + " score = " + score;
+    }
+}
