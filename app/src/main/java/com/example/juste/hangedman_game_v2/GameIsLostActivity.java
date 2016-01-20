227cbd698c80cfd1e45535d8a8b462965176e978
STENILO <stenilo@gmail.com>
2016-01-20 12:32:21 +0100

    Fragments, not connected

--- app/src/main/java/com/example/juste/hangedman_game_v2/GameIsLostActivity.java
+++ app/src/main/java/com/example/juste/hangedman_game_v2/GameIsLostActivity.java
@@ -1,6 +1,7 @@
 package com.example.juste.hangedman_game_v2;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +10,7 @@ import android.widget.TextView;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public class GameIsLostActivity extends hangedmanGame implements View.OnClickListener{
+public class GameIsLostActivity extends hangedmanGame implements View.OnClickListener {
     private TextView gameIsLostText;
     private Button returnToMain;
     String word;
